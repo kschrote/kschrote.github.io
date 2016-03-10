5d790f4cf3b147706fb30542324ea5d61a80c68d
kschrote <kjschroter@yahoo.com>
2016-03-10 07:11:08 +0000

    exquisite

--- pattern/script.js
+++ pattern/script.js
@@ -2,17 +2,16 @@ var heads = ["moonhead.jpg", "goku.jpg", "lisahead.jpg", "otterhead.jpg", "dog.j
 var torso = ["lisatorso.jpg", "misprints.jpg", "moontorso.jpg", "sherlocktorso.jpg", "tree.jpg"];
 var legs = ["crocs.jpg", "lisalegs.jpg", "moonlegs.jpg", "octopus.jpg", "spongebob.jpg"];
 
-$("mybutton").click(function(){
+$(".mybutton").click(function(){
         var randomhead = heads[Math.floor(Math.random()*heads.length)];
         $(".head").html("<img src='images/" + randomhead + "'>");
-        
+
         var randomtorso = torso[Math.floor(Math.random()*torso.length)];
         $(".torso").html("<img src='images/" + randomtorso + "'>");
 
         var randomlegs = legs[Math.floor(Math.random()*legs.length)];
         $(".legs").html("<img src='images/" + randomlegs + "'>");
-});
-            
+});         
             
             
             
